09c88a1e363cb8438ba6f79e2f7774f27ec41b51
PoliComo <nguyenhuukien2909@gmail.com>
2025-07-24 15:53:53 +0700

    Sửa đổi vị trí 2 nút

--- posawesome/public/js/posapp/components/pos/ClosingDialog.vue
+++ posawesome/public/js/posapp/components/pos/ClosingDialog.vue
@@ -82,27 +82,27 @@
 
 				<v-divider></v-divider>
 				<v-card-actions class="dialog-actions-container">
-					<v-btn
-						theme="dark"
-						@click="submit_dialog"
-						class="pos-action-btn submit-action-btn"
-						size="large"
-						elevation="2"
-					>
-						<v-icon start>mdi-check-circle-outline</v-icon>
-						<span>{{ __("Submit") }}</span>
-					</v-btn>
-					<v-spacer></v-spacer>
-					<v-btn
-						theme="dark"
-						@click="close_dialog"
-						class="pos-action-btn cancel-action-btn"
-						size="large"
-						elevation="2"
-					>
-						<v-icon start>mdi-close-circle-outline</v-icon>
-						<span>{{ __("Close") }}</span>
-					</v-btn>
+				<v-btn
+					theme="dark"
+					@click="close_dialog"
+					class="pos-action-btn cancel-action-btn"
+					size="large"
+					elevation="2"
+				>
+					<v-icon start>mdi-close-circle-outline</v-icon>
+					<span>{{ __("Close") }}</span>
+				</v-btn>
+				<v-spacer></v-spacer>
+				<v-btn
+					theme="dark"
+					@click="submit_dialog"
+					class="pos-action-btn submit-action-btn"
+					size="large"
+					elevation="2"
+				>
+					<v-icon start>mdi-check-circle-outline</v-icon>
+					<span>{{ __("Submit") }}</span>
+				</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-dialog>
