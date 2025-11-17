57b8a8344bd768e69ac2d3fbf4ab9ce255cc5db3
PoliComo <nguyenhuukien2909@gmail.com>
2025-11-17 20:18:58 +0700

    Add Bank QR Code

--- posawesome/public/js/posapp/components/pos/Payments.vue
+++ posawesome/public/js/posapp/components/pos/Payments.vue
@@ -813,7 +813,6 @@ import {
 
 import generateOfflineInvoiceHTML from "../../../offline_print_template";
 import { silentPrint } from "../../plugins/print.js";
-import md5 from 'crypto-js/md5';
 
 export default {
 	// Using format mixin for shared formatting methods
@@ -1157,13 +1156,165 @@ export default {
 			const amount = this.invoice_doc.grand_total || this.invoice_doc.rounded_total || 0;
 			const desc = this.invoice_doc.name || '';
 			const hashInput = amount.toString() + desc;
-			const hash = md5(hashInput).toString();
+			const hash = this.md5(hashInput);
 			const url = `https://pay.vtmart.online/?p?amount=${amount}&desc=${desc}&Hash=${hash}`;
 			window.open(url, '_blank');
 		},
 		// Mark payment as confirmed manually
 		markAsPaid() {
 			this.is_payment_confirmed = true;
+		},
+		// MD5 hash function implementation
+		md5(string) {
+			function addUnsigned(lX, lY) {
+				var lX4, lY4, lX8, lY8, lResult;
+				lX8 = (lX & 0x80000000);
+				lY8 = (lY & 0x80000000);
+				lX4 = (lX & 0x40000000);
+				lY4 = (lY & 0x40000000);
+				lResult = (lX & 0x3FFFFFFF) + (lY & 0x3FFFFFFF);
+				if (lX4 & lY4) {
+					return (lResult ^ 0x80000000 ^ lX8 ^ lY8);
+				}
+				if (lX4 | lY4) {
+					if (lResult & 0x40000000) {
+						return (lResult ^ 0xC0000000 ^ lX8 ^ lY8);
+					} else {
+						return (lResult ^ 0x40000000 ^ lX8 ^ lY8);
+					}
+				} else {
+					return (lResult ^ lX8 ^ lY8);
+				}
+			}
+			function F(x, y, z) { return (x & y) | (~x & z); }
+			function G(x, y, z) { return (x & z) | (y & ~z); }
+			function H(x, y, z) { return (x ^ y ^ z); }
+			function I(x, y, z) { return (y ^ (x | ~z)); }
+			function FF(a, b, c, d, x, s, ac) {
+				a = addUnsigned(a, addUnsigned(addUnsigned(F(b, c, d), x), ac));
+				return addUnsigned(leftRotate(a, s), b);
+			}
+			function GG(a, b, c, d, x, s, ac) {
+				a = addUnsigned(a, addUnsigned(addUnsigned(G(b, c, d), x), ac));
+				return addUnsigned(leftRotate(a, s), b);
+			}
+			function HH(a, b, c, d, x, s, ac) {
+				a = addUnsigned(a, addUnsigned(addUnsigned(H(b, c, d), x), ac));
+				return addUnsigned(leftRotate(a, s), b);
+			}
+			function II(a, b, c, d, x, s, ac) {
+				a = addUnsigned(a, addUnsigned(addUnsigned(I(b, c, d), x), ac));
+				return addUnsigned(leftRotate(a, s), b);
+			}
+			function leftRotate(value, amount) {
+				return (value << amount) | (value >>> (32 - amount));
+			}
+			function convertToWordArray(string) {
+				var messageLength = string.length;
+				var numberOfWords_temp1 = messageLength + 8;
+				var numberOfWords_temp2 = (numberOfWords_temp1 - (numberOfWords_temp1 % 64)) / 64;
+				var numberOfWords = (numberOfWords_temp2 + 1) * 16;
+				var wordArray = new Array(numberOfWords - 1);
+				var bytePosition = 0;
+				var byteCount = 0;
+				while (byteCount < messageLength) {
+					var count = (byteCount - (byteCount % 4)) / 4;
+					bytePosition = (byteCount % 4) * 8;
+					wordArray[count] = (wordArray[count] | (string.charCodeAt(byteCount) << bytePosition));
+					byteCount++;
+				}
+				var count2 = (byteCount - (byteCount % 4)) / 4;
+				bytePosition = (byteCount % 4) * 8;
+				wordArray[count2] = wordArray[count2] | (0x80 << bytePosition);
+				wordArray[numberOfWords - 2] = messageLength << 3;
+				wordArray[numberOfWords - 1] = messageLength >>> 29;
+				return wordArray;
+			}
+			function wordToHex(value) {
+				var hex = '';
+				for (var i = 0; i <= 3; i++) {
+					var byte = (value >>> (i * 8)) & 255;
+					hex += ('0' + byte.toString(16)).slice(-2);
+				}
+				return hex;
+			}
+			var x = convertToWordArray(string);
+			var a = 0x67452301;
+			var b = 0xEFCDAB89;
+			var c = 0x98BADCFE;
+			var d = 0x10325476;
+			for (var i = 0; i < x.length; i += 16) {
+				var AA = a, BB = b, CC = c, DD = d;
+				a = FF(a, b, c, d, x[i + 0], 7, 0xD76AA478);
+				d = FF(d, a, b, c, x[i + 1], 12, 0xE8C7B756);
+				c = FF(c, d, a, b, x[i + 2], 17, 0x242070DB);
+				b = FF(b, c, d, a, x[i + 3], 22, 0xC1BDCEEE);
+				a = FF(a, b, c, d, x[i + 4], 7, 0xF57C0FAF);
+				d = FF(d, a, b, c, x[i + 5], 12, 0x4787C62A);
+				c = FF(c, d, a, b, x[i + 6], 17, 0xA8304613);
+				b = FF(b, c, d, a, x[i + 7], 22, 0xFD469501);
+				a = FF(a, b, c, d, x[i + 8], 7, 0x698098D8);
+				d = FF(d, a, b, c, x[i + 9], 12, 0x8B44F7AF);
+				c = FF(c, d, a, b, x[i + 10], 17, 0xFFFF5BB1);
+				b = FF(b, c, d, a, x[i + 11], 22, 0x895CD7BE);
+				a = FF(a, b, c, d, x[i + 12], 7, 0x6B901122);
+				d = FF(d, a, b, c, x[i + 13], 12, 0xFD987193);
+				c = FF(c, d, a, b, x[i + 14], 17, 0xA679438E);
+				b = FF(b, c, d, a, x[i + 15], 22, 0x49B40821);
+				a = GG(a, b, c, d, x[i + 1], 5, 0xF61E2562);
+				d = GG(d, a, b, c, x[i + 6], 9, 0xC040B340);
+				c = GG(c, d, a, b, x[i + 11], 14, 0x265E5A51);
+				b = GG(b, c, d, a, x[i + 0], 20, 0xE9B6C7AA);
+				a = GG(a, b, c, d, x[i + 5], 5, 0xD62F105D);
+				d = GG(d, a, b, c, x[i + 10], 9, 0x02441453);
+				c = GG(c, d, a, b, x[i + 15], 14, 0xD8A1E681);
+				b = GG(b, c, d, a, x[i + 4], 20, 0xE7D3FBC8);
+				a = GG(a, b, c, d, x[i + 9], 5, 0x21E1CDE6);
+				d = GG(d, a, b, c, x[i + 14], 9, 0xC33707D6);
+				c = GG(c, d, a, b, x[i + 3], 14, 0xF4D50D87);
+				b = GG(b, c, d, a, x[i + 8], 20, 0x455A14ED);
+				a = GG(a, b, c, d, x[i + 13], 5, 0xA9E3E905);
+				d = GG(d, a, b, c, x[i + 2], 9, 0xFCEFA3F8);
+				c = GG(c, d, a, b, x[i + 7], 14, 0x676F02D9);
+				b = GG(b, c, d, a, x[i + 12], 20, 0x8D2A4C8A);
+				a = HH(a, b, c, d, x[i + 5], 4, 0xFFFA3942);
+				d = HH(d, a, b, c, x[i + 8], 11, 0x8771F681);
+				c = HH(c, d, a, b, x[i + 11], 16, 0x6D9D6122);
+				b = HH(b, c, d, a, x[i + 14], 23, 0xFDE5380C);
+				a = HH(a, b, c, d, x[i + 1], 4, 0xA4BEEA44);
+				d = HH(d, a, b, c, x[i + 4], 11, 0x4BDECFA9);
+				c = HH(c, d, a, b, x[i + 7], 16, 0xF6BB4B60);
+				b = HH(b, c, d, a, x[i + 10], 23, 0xBEBFBC70);
+				a = HH(a, b, c, d, x[i + 13], 4, 0x289B7EC6);
+				d = HH(d, a, b, c, x[i + 0], 11, 0xEAA127FA);
+				c = HH(c, d, a, b, x[i + 3], 16, 0xD4EF3085);
+				b = HH(b, c, d, a, x[i + 6], 23, 0x04881D05);
+				a = HH(a, b, c, d, x[i + 9], 4, 0xD9D4D039);
+				d = HH(d, a, b, c, x[i + 12], 11, 0xE6DB99E5);
+				c = HH(c, d, a, b, x[i + 15], 16, 0x1FA27CF8);
+				b = HH(b, c, d, a, x[i + 2], 23, 0xC4AC5665);
+				a = II(a, b, c, d, x[i + 0], 6, 0xF4292244);
+				d = II(d, a, b, c, x[i + 7], 10, 0x432AFF97);
+				c = II(c, d, a, b, x[i + 14], 15, 0xAB9423A7);
+				b = II(b, c, d, a, x[i + 5], 21, 0xFC93A039);
+				a = II(a, b, c, d, x[i + 12], 6, 0x655B59C3);
+				d = II(d, a, b, c, x[i + 3], 10, 0x8F0CCC92);
+				c = II(c, d, a, b, x[i + 10], 15, 0xFFEFF47D);
+				b = II(b, c, d, a, x[i + 1], 21, 0x85845DD1);
+				a = II(a, b, c, d, x[i + 8], 6, 0x6FA87E4F);
+				d = II(d, a, b, c, x[i + 15], 10, 0xFE2CE6E0);
+				c = II(c, d, a, b, x[i + 6], 15, 0xA3014314);
+				b = II(b, c, d, a, x[i + 13], 21, 0x4E0811A1);
+				a = II(a, b, c, d, x[i + 4], 6, 0xF7537E82);
+				d = II(d, a, b, c, x[i + 11], 10, 0xBD3AF235);
+				c = II(c, d, a, b, x[i + 2], 15, 0x2AD7D2BB);
+				b = II(b, c, d, a, x[i + 9], 21, 0xEB86D391);
+				a = addUnsigned(a, AA);
+				b = addUnsigned(b, BB);
+				c = addUnsigned(c, CC);
+				d = addUnsigned(d, DD);
+			}
+			return (wordToHex(a) + wordToHex(b) + wordToHex(c) + wordToHex(d)).toLowerCase();
 		},
 		// Reset all cash payments to zero
 		reset_cash_payments() {
